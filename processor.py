93de3ba1ef660015ee231413d808d0a3890e7f38
Christina Reissel <christina.reissel@cern.ch>
2026-01-30 16:32:32 -0500

    save outputs for ML #1

--- processor.py
+++ processor.py
@@ -12,6 +12,7 @@ class MyProcessor(processor.ProcessorABC):
                 hist.axis.Integer(0, 10, name="nLeptons", label="Number of leptons (muons)"),
                 storage=hist.storage.Weight(),
                 ),
+            "array": processor.column_accumulator(ak.Array([]))
         }
 
     @property
@@ -19,13 +20,8 @@ class MyProcessor(processor.ProcessorABC):
         return self._accumulator
 
     def process(self, events):
-        out = {
-            "events": processor.defaultdict_accumulator(int),
-            "nLeptons": hist.Hist(
-                hist.axis.Integer(0, 10, name="nLeptons", label="Number of leptons (muons)"),
-                storage=hist.storage.Weight(),
-                ),
-        }
+        out = self.accumulator.copy()
+        
         isMC = events.metadata["isMC"]
         out["events"]["all"] += len(events)
 
@@ -57,7 +53,26 @@ class MyProcessor(processor.ProcessorABC):
         out["nLeptons"].fill(nLeptons=n_muons)
         out["events"]["selected"] += len(events[cut])
 
+        # save basic event identification & monitoring variables
+        selected_events = events[cut]
+        variables = {
+                        "event": selected_events.event,
+                        "run": selected_events.run,
+                        "luminosityBlock": selected_events.luminosityBlock,
+        }
+        if isMC:
+               variables["weight"] =  events.genWeight
+        # add the selected muon kinematics
+        selected_muons = muons[cut]
+        for i in range(4):
+            variables[f"pt{i}"] = selected_muons.pt[:,i]
+            variables[f"eta{i}"] = selected_muons.eta[:,i]
+            variables[f"phi{i}"] = selected_muons.phi[:,i]
+
+        out["array"] += processor.column_accumulator(ak.zip(variables, depth_limit=1))
         return out
 
     def postprocess(self, accumulator):
+        if len(accumulator["array"]) > 0:
+            ak.to_parquet(accumulator["array"].value, "test", overwrite=True)
         return accumulator
