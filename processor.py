d1e2bf1e041fc66e0e940dbd59c635675dd39c08
Christina Reissel <christina.reissel@cern.ch>
2026-01-30 14:27:53 -0500

    implement basic object & event selection

--- processor.py
+++ processor.py
@@ -1,4 +1,5 @@
 from coffea import processor
+from coffea.analysis_tools import PackedSelection
 import awkward as ak
 import hist
 from utils import *
@@ -7,8 +8,8 @@ class MyProcessor(processor.ProcessorABC):
     def __init__(self):
         self._accumulator = {
             "events": processor.defaultdict_accumulator(int),
-            "nMuons": hist.Hist(
-                hist.axis.Integer(0, 10, name="nMuons", label="Number of muons"),
+            "nLeptons": hist.Hist(
+                hist.axis.Integer(0, 10, name="nLeptons", label="Number of leptons (muons)"),
                 storage=hist.storage.Weight(),
                 ),
         }
@@ -20,21 +21,41 @@ class MyProcessor(processor.ProcessorABC):
     def process(self, events):
         out = {
             "events": processor.defaultdict_accumulator(int),
-            "nMuons": hist.Hist(
-                hist.axis.Integer(0, 10, name="nMuons", label="Number of muons"),
+            "nLeptons": hist.Hist(
+                hist.axis.Integer(0, 10, name="nLeptons", label="Number of leptons (muons)"),
                 storage=hist.storage.Weight(),
-                )
+                ),
         }
-
+        isMC = events.metadata["isMC"]
         out["events"]["all"] += len(events)
 
-        # event filtering and triggers are missing!
-        # all MC/data corrections are missing!
+        # event filtering and triggers
+        flags = (events.Flag.goodVertices & events.Flag.globalSuperTightHalo2016Filter & events.Flag.HBHENoiseFilter & events.Flag.HBHENoiseIsoFilter & events.Flag.EcalDeadCellTriggerPrimitiveFilter & events.Flag.BadPFMuonFilter & events.Flag.BadChargedCandidateFilter & events.Flag.ecalBadCalibFilter)
+        if not isMC:
+            flags = flags & events.Flag.eeBadScFilter
+        triggers = (events.HLT.Mu17_TrkIsoVVL_Mu8_TrkIsoVVL | events.HLT.Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL | events.HLT.TripleMu_12_10_5)
+        # trigger vetos for electon channels missing!
 
-        # needs reasonable event selection & reconstruction HERE!
+        # simplified muon selection compared to CMS AN2016_442
         muons = select_muons(events)
         n_muons = ak.num(muons)
-        out["nMuons"].fill(nMuons=n_muons)
+
+        # event selection
+        selc = PackedSelection()
+        selc.add_multiple(
+                {
+                    "Filter": flags,
+                    "Triggers": triggers,
+                    "Lepton selection":  (n_muons > 3),
+                }
+        )
+        print(selc)
+        cutflow = selc.cutflow("Filter", "Triggers", "Lepton selection")
+        cutflow.print()
+        cut = selc.all()
+       
+        out["nLeptons"].fill(nLeptons=n_muons)
+        out["events"]["selected"] += len(events[cut])
 
         return out
 
--- utils.py
+++ utils.py
@@ -12,8 +12,16 @@ def select_electrons(events):
     # can be updated with different WP etc.
     return el[base & (el.cutBased >= 3)]
 
-def make_lepton_pair(leptons):
+Z_MASS = 91.1876
+Z_CANDIDATE_MASS_MIN = 12.0
+Z_CANDIDATE_MASS_MAX = 120.0
+FOUR_LEPTON_MASS_MIN = 70.0
+
+def make_Z_candidates(leptons):
+
+    # make all possible Z boson combinations
     pair = ak.combinations(leptons, 2, fields=["l1", "l2"])
-    pair = pair[pair.l1.charge != pair.l2.charge]
-    num_pair = ak.num(pair)
-    return pair, num_pair
+    opp_charge = (pair.l1.charge != pair.l2.charge)
+    mass = (pair.l1 + pair.l2).mass
+    z_mask = opp_charge & (mass > Z_CANDIDATE_MASS_MIN) & (mass < Z_CANDIDATE_MASS_MAX)
+    return pair[z_mask]
