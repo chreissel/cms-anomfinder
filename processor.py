4b10752e36f068a0df691aae9c8034bb710dc549
Christina Reissel <christina.reissel@cern.ch>
2026-02-03 11:49:12 -0500

    save outputs for ML #2

--- processor.py
+++ processor.py
@@ -3,28 +3,19 @@ from coffea.analysis_tools import PackedSelection
 import awkward as ak
 import hist
 from utils import *
+import dask_awkward as dak
+import dask
+from coffea.processor import column_accumulator
 
 class MyProcessor(processor.ProcessorABC):
     def __init__(self):
-        self._accumulator = {
-            "events": processor.defaultdict_accumulator(int),
-            "nLeptons": hist.Hist(
-                hist.axis.Integer(0, 10, name="nLeptons", label="Number of leptons (muons)"),
-                storage=hist.storage.Weight(),
-                ),
-            "array": processor.column_accumulator(ak.Array([]))
-        }
-
-    @property
-    def accumulator(self):
-        return self._accumulator
+        pass
 
     def process(self, events):
-        out = self.accumulator.copy()
-        
-        isMC = events.metadata["isMC"]
-        out["events"]["all"] += len(events)
 
+        isMC = events.metadata["isMC"]
+        dataset = events.metadata["dataset"]
+        
         # event filtering and triggers
         flags = (events.Flag.goodVertices & events.Flag.globalSuperTightHalo2016Filter & events.Flag.HBHENoiseFilter & events.Flag.HBHENoiseIsoFilter & events.Flag.EcalDeadCellTriggerPrimitiveFilter & events.Flag.BadPFMuonFilter & events.Flag.BadChargedCandidateFilter & events.Flag.ecalBadCalibFilter)
         if not isMC:
@@ -45,14 +36,9 @@ class MyProcessor(processor.ProcessorABC):
                     "Lepton selection":  (n_muons > 3),
                 }
         )
-        print(selc)
         cutflow = selc.cutflow("Filter", "Triggers", "Lepton selection")
-        cutflow.print()
         cut = selc.all()
        
-        out["nLeptons"].fill(nLeptons=n_muons)
-        out["events"]["selected"] += len(events[cut])
-
         # save basic event identification & monitoring variables
         selected_events = events[cut]
         variables = {
@@ -61,18 +47,26 @@ class MyProcessor(processor.ProcessorABC):
                         "luminosityBlock": selected_events.luminosityBlock,
         }
         if isMC:
-               variables["weight"] =  events.genWeight
+               variables["weight"] =  selected_events.genWeight
         # add the selected muon kinematics
         selected_muons = muons[cut]
         for i in range(4):
             variables[f"pt{i}"] = selected_muons.pt[:,i]
             variables[f"eta{i}"] = selected_muons.eta[:,i]
             variables[f"phi{i}"] = selected_muons.phi[:,i]
+            variables[f"pdgId{i}"] = selected_muons.pdgId[:,i]
+            # displacement variables
+            variables[f"dz{i}"] = selected_muons.dz[:,i]
+            variables[f"dxy{i}"] = selected_muons.dxy[:,i]
 
-        out["array"] += processor.column_accumulator(ak.zip(variables, depth_limit=1))
-        return out
+        
+            
+        return {
+            dataset: {
+                "cutflow": {"all": len(events), "selected": len(events[cut])}, 
+                "array": column_accumulator(ak.zip(variables, depth_limit=1))
+            }
+        }
 
     def postprocess(self, accumulator):
-        if len(accumulator["array"]) > 0:
-            ak.to_parquet(accumulator["array"].value, "test", overwrite=True)
         return accumulator
